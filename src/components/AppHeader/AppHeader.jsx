67e7df78e2a9cdeac935b961cab3981dd1a195e2
Kanako-muff <kanako.design00@gmail.com>
2023-08-23 20:49:57 -0700

    added Dark Mode

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import styles from "../../styles/modules/app.module.scss";
 import { SelectButton } from "../Button/Button";
 import { TodoModal } from "../TodoModal/TodoModal";
@@ -9,11 +9,15 @@ import { updateFilterStatus } from "../Slices/todoSlices";
 export const AppHeader = () => {
   const [modalOpen, setModalOpen] = useState(false);
   const filterStatus = useSelector((state) => state.todo.filterStatus);
-  // const [filterStatus, setFilterStatus] = useState(initialFilterStatus);
   const dispatch = useDispatch();
 
   const updateFilter = (e) => {
     dispatch(updateFilterStatus(e.target.value));
+  };
+
+  const toggleDarkMode = () => {
+    const htmlElement = document.documentElement;
+    htmlElement.classList.toggle("dark-mode");
   };
 
   return (
@@ -26,6 +30,9 @@ export const AppHeader = () => {
         <option value="incomplete">Incomplete</option>
         <option value="complete">complete</option>
       </SelectButton>
+      <Button variant="secondary" onClick={toggleDarkMode}>
+        Toggle Dark Mode
+      </Button>
       <TodoModal type="add" modalOpen={modalOpen} setModalOpen={setModalOpen} />
     </div>
   );
